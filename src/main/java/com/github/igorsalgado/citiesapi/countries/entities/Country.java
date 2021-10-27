ed7657b9dd8b9f73c2da4722bd383c36f0fc7434
Igor Salgado <salgado.igor.is@gmail.com>
2021-10-27 13:46:49 -0300

    Mapeando tabela Pais

---
+++ src/main/java/com/github/igorsalgado/citiesapi/countries/entities/Country.java
@@ -0,0 +1,36 @@
+package com.github.igorsalgado.citiesapi.countries.entities;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Table(name = "pais")
+public class Country {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "nome_pt")
+    private String portugueseName;
+
+    @Column(name = "sigla")
+    private String code;
+
+    private Integer bacen;
+
+
+}
---
+++ src/main/java/com/github/igorsalgado/citiesapi/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.github.igorsalgado.citiesapi.repository;
+
+import com.github.igorsalgado.citiesapi.countries.entities.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country,Long> {
+}
