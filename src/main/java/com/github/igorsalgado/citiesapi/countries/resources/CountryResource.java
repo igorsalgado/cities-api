1f6a59001bf69bb007aefe3077ad9e75dac0029a
Igor Salgado <salgado.igor.is@gmail.com>
2021-10-27 15:27:02 -0300

    Criando paginação

--- src/main/java/com/github/igorsalgado/citiesapi/countries/resources/CountryResource.java
+++ src/main/java/com/github/igorsalgado/citiesapi/countries/resources/CountryResource.java
@@ -3,11 +3,12 @@ package com.github.igorsalgado.citiesapi.countries.resources;
 import com.github.igorsalgado.citiesapi.countries.entities.Country;
 import com.github.igorsalgado.citiesapi.countries.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
@@ -17,7 +18,7 @@ public class CountryResource {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries() {
-        return repository.findAll();
+    public Page<Country> countries(Pageable page) {
+        return repository.findAll(page);
     }
 }
