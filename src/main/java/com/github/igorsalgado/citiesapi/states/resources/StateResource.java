df73f83c17aad4e7385821a96dc62f0988ea2fba
Igor Salgado <salgado.igor.is@gmail.com>
2021-10-27 20:25:52 -0300

    Mapeando tabela Estado

--- src/main/java/com/github/igorsalgado/citiesapi/states/resources/StateResource.java
+++ src/main/java/com/github/igorsalgado/citiesapi/states/resources/StateResource.java
@@ -1,12 +1,24 @@
 package com.github.igorsalgado.citiesapi.states.resources;
 
+import com.github.igorsalgado.citiesapi.states.entities.State;
 import com.github.igorsalgado.citiesapi.states.repositories.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/states")
 public class StateResource {
+
+    @Autowired
     private StateRepository repository;
+
+    @GetMapping
+    public List<State> states(){
+        return repository.findAll();
+    }
 }
